8e62b949b73a2037973963293fce5a5c063cc297
Dylan Curtis <dcurtisj@gmail.com>
2023-08-29 16:42:35 -0700

    update user page

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -24,6 +24,7 @@ function UserStatPage({user, setBackground}) {
         { user ? 
           <div>
             <h3 id="no-hover">{user.name}</h3><Link to='/edit-user'><h3>Edit Account </h3></Link>
+            <h3 id="user-profile">{user.blurb}<h3>{user.blurb}</h3>Edit your profile to add an about me section</h3>
           </div> 
           : "" }
         <span className ="">            
