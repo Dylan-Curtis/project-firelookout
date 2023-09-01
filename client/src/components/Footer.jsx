41c3c456e45676988ac94538b676c754d527360a
Dylan Curtis <dcurtisj@gmail.com>
2023-08-31 22:04:04 -0700

    last day commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer( ) {
 
 
     return (
-        <footer>  
+        <footer className= "foot">  
               <h1 className="LookoutTrails-footer"><strong>Lookout Trails</strong></h1>
               <Link to="/" className="HikeATrail-footer"> <strong>Hike a Trail</strong></Link>
             <Link to="/create-trail" className="CreateATrail-footer"> <strong> Create a Trail</strong></Link>  
