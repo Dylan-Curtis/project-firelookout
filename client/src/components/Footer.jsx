7343233a2ebdfdb4527b6acc1d4fabfbcac23e30
Dylan Curtis <dcurtisj@gmail.com>
2023-09-01 12:05:01 -0700

    fix footer background and bottom links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,12 +5,17 @@ function Footer( ) {
 
 
     return (
-        <footer className= "foot">  
-              <h1 className="LookoutTrails-footer"><strong>Lookout Trails</strong></h1>
+        <footer >  
+        <h1 className= "foot">
+        <div className="footer-links">
+            <h1 className="LookoutTrails-footer"><strong>Lookout Trails</strong></h1>
+            
               <Link to="/" className="HikeATrail-footer"> <strong>Hike a Trail</strong></Link>
-            <Link to="/create-trail" className="CreateATrail-footer"> <strong> Create a Trail</strong></Link>  
+              <Link to="/create-trail" className="CreateATrail-footer"> <strong> Create a Trail</strong></Link>  
+            </div>
+            </h1>
         </footer>
       ) 
     }
     
-    export default Footer
+    export default Footer 
