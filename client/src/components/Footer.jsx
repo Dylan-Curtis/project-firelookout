d1ced7ff491ee09ad1e67eff5bb4c59aecc2eade
Dylan Curtis <dcurtisj@gmail.com>
2023-09-01 13:32:09 -0700

    complete footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import FootImage from '../images/FootImage.png'
 
 function Footer( ) {   
 
@@ -7,9 +8,9 @@ function Footer( ) {
     return (
         <footer >  
         <h1 className= "foot">
+        <img  className="lookoutImageFooter" alt="lookoutLogo" src={FootImage}></img>
         <div className="footer-links">
-            <h1 className="LookoutTrails-footer"><strong>Lookout Trails</strong></h1>
-            
+            <h1 className="LookoutTrails-footer"><strong>Lookout Trails</strong></h1>            
               <Link to="/" className="HikeATrail-footer"> <strong>Hike a Trail</strong></Link>
               <Link to="/create-trail" className="CreateATrail-footer"> <strong> Create a Trail</strong></Link>  
             </div>
