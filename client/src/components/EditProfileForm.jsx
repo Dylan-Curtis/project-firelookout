2cb3fd4880a7551955a8a03d5db618e86588adf1
Dylan Curtis <dcurtisj@gmail.com>
2023-09-21 21:38:44 -0700

    end of day push

---
+++ client/src/components/EditProfileForm.jsx
@@ -0,0 +1,92 @@
+import {Link} from 'react-router-dom'
+import {useState} from 'react'
+import LookoutLogo from '../images/LookoutLogo.png'
+import { useNavigate } from "react-router-dom";
+import SignInBackground from '../images/SignInBackground.png'
+function EditProfileForm({ onLogin, setErrors, errors }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const backgroundStyle = {
+      backgroundImage: `url(${SignInBackground})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      minHeight: '100vh', // Ensure the container takes up the entire viewport height
+      display: 'flex',
+      alignItems: 'center', // Vertically center the form
+      justifyContent: 'center', // Horizontally center the form
+      padding: '20px', // Add some padding around the form
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();       
+        fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }).then((r) => {           
+            if (r.ok) {
+              r.json().then((user) => onLogin(user));
+              navigate("/");   
+            } else {
+              r.json().then((err) => setErrors([err.error]));             
+            }
+
+            setTimeout(() => {
+              setErrors([]);
+            }, 5000);
+          });
+        }
+        return (
+          <div className="container" style={backgroundStyle}>
+        
+            <form onSubmit={handleSubmit} class= "form">
+            <img  className="lookoutImageForm" alt="lookoutLogo" src={LookoutLogo}></img>
+
+               {Array.isArray(errors) &&
+            errors.map((err) => (
+              <p key={err} style={{ color: 'red' }}>
+                {err}
+              </p>
+            ))}
+              <div class="title">Welcome Back!</div>
+             
+                {/* <label htmlFor="email">Email</label> */}
+                <input
+                  type="text"
+                  id="email"
+                  autoComplete="off"
+                  class="input-container"
+                  value={email}
+                  placeholder="Email Address"
+                  onChange={(e) => setEmail(e.target.value)}
+                /> 
+            
+             
+                <label htmlFor="password"></label>
+                <input
+                  type="password"
+                  id="password"
+                  class="input-container"
+                  autoComplete="current-password"
+                  value={password}
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />                       
+             
+             
+                <button type="submit" class="submit">Submit</button>
+                <div class="subtitle">Don't Have an Account?<Link to="/signup"> Sign Up!</Link></div>
+              
+                <div className="TOSNotice">
+                  By continuing to use Lookout Trails, you agree to our <strong>Terms of Service</strong> and <strong>Privacy Policy.</strong>  
+                </div>
+            </form>
+            </div>
+          );
+        }
+        export default EditProfileForm
--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,20 +1,59 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import EditProfileForm from "./EditProfileForm";
 
-function UserStatPage({ user }) {
+function UserProfile({ user }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedName, setEditedName] = useState(user.name);
+  const [editedBlurb, setEditedBlurb] = useState(user.blurb);
+
+  const handleEditToggle = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleSave = () => {
+    // Perform save operation here with editedName and editedBlurb
+    // For example, you can send a PUT request to update the user's profile on the server.
+    // After successful update, you can set isEditing to false.
+    // You should implement this part based on your backend and state management.
+
+    setIsEditing(false); // For demonstration purposes, we're just toggling isEditing to false.
+  };
+
   return (
-    <div className="user-container">      
-        <div className="user-title">
-          <h3 className="title">
-            {user.name}{" "}
-            <button className="smallButton">Edit Account</button>
-          </h3>
-          <p id="user-profile">{user.blurb}</p>
-          <p>Edit your profile to add an about me section</p>
-        </div>      
+    <div className="user-container">
+      <div className="user-title">
+        {isEditing ? (
+          <>
+            <h3 className="title">
+              <input
+                type="text"
+                value={editedName}
+                onChange={(e) => setEditedName(e.target.value)}
+              />
+            </h3>
+            <div className="button-container">
+              <EditProfileForm onSave={handleSave} onCancel={handleEditToggle} />
+              <button onClick={handleEditToggle} className="smallButton">
+                Cancel
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="button-container">
+              <h3 className="title">{user.name}</h3>
+              <button onClick={handleEditToggle} className="smallButton">
+                Edit Account
+              </button>
+            </div>
+            <p id="user-profile">{user.blurb}</p>
+            {!isEditing && <p>Edit your profile to add an about me section</p>}
+          </>
+        )}
+      </div>
       <span className=""></span>
     </div>
   );
 }
 
-export default UserStatPage;
+export default UserProfile;
